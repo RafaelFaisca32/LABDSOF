ade4b7861441f3a3244e19e342a6a63dbfcc552a
Nuno Marmeleiro <1190922@isep.ipp.pt>
2024-10-31 19:42:37 +0000

    LABDSOF-189 Backend Base init

---
+++ netquest-backend/src/test/java/com/labdsof/netquest_backend/NetquestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labdsof.netquest_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetquestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
