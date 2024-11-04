bae4dd108a025174680b3d653c0a2fa0e7fcfd51
Rafael Faísca <1180658@isep.ipp.pt>
2024-11-04 23:53:34 +0000

    LABDSOF-194: Docker repo and dockerfile configuration

--- netquest-frontend/src/Constants.js
+++ netquest-frontend/src/Constants.js
@@ -1,5 +1,11 @@
-export const config = {
+const prod = {
+  url: {
+    API_BASE_URL: "https://netquest-dev-environment.onrender.com",
+  },
+};
+const dev = {
   url: {
     API_BASE_URL: "http://localhost:8080",
   },
 };
+export const config = process.env.NODE_ENV === "development" ? dev : prod;
