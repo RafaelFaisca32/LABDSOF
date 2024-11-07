ca14a67d463af50f2f3620d842ca9278d94ee6b1
Rafael Faísca <1180658@isep.ipp.pt>
2024-11-07 00:45:15 +0000

    fix local config

--- netquest-frontend/src/Constants.js
+++ netquest-frontend/src/Constants.js
@@ -1,21 +1,5 @@
-const prod = {
-  url: {
-    API_BASE_URL: "https://netquest-labdsof-prod.onrender.com/",
-  },
-};
-const local = {
+export const config = {
   url: {
     API_BASE_URL: "http://localhost:8080",
   },
 };
-const dev = {
-  url: {
-    API_BASE_URL: "https://netquest-dev-environment.onrender.com",
-  },
-};
-export const config =
-  process.env.NODE_ENV === "development"
-    ? local
-    : process.env.NODE_ENV === "prod"
-      ? prod
-      : dev;
